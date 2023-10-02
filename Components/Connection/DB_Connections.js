328243ef324cf41ef8bacd79a4e420f46cca7337
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2023-10-02 10:42:45 +0530

    testing commit

--- Components/Connection/DB_Connections.js
+++ Components/Connection/DB_Connections.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const connection =  () => {
     // connect to local database 
-    const connect=mongoose.connect("mongodb://127.0.0.1:27017/andc_treasure")
-    // const connect=mongoose.connect("mongodb+srv://khanshahnwaz:Anonymous786@andctreasure.e5tjaqw.mongodb.net/andctreasure?retryWrites=true&w=majority")
+    // const connect=mongoose.connect("mongodb://127.0.0.1:27017/andc_treasure")
+    const connect=mongoose.connect("mongodb+srv://khanshahnwaz:Anonymous786@andctreasure.e5tjaqw.mongodb.net/andctreasure?retryWrites=true&w=majority")
     if (connect) {
         console.log("Connection successfull.")
     } else {
